56c972bfe53037061be6f4b9c2591c1b81b0391d
Anurag Lal <anurag1995lal@gmail.com>
2025-06-04 23:31:50 +0530

    Enabled logging and use of @Pathvariable.

--- restful-web-services/src/main/java/com/rest/webservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/rest/webservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.rest.webservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public helloWorldBean helloWorldBean() {
         return new  helloWorldBean ("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public helloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+        return new  helloWorldBean(String.format ("Hello World , %s", name));
     }
 }
