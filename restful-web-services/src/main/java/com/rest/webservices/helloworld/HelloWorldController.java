1c15eccfd47fb27ddaa3905d4cab46dd6f36bd7c
Anurag Lal <anurag1995lal@gmail.com>
2025-06-05 15:23:08 +0530

    internationalization -i18n, Accept-Language header fr.

--- restful-web-services/src/main/java/com/rest/webservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/rest/webservices/helloworld/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.rest.webservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
@@ -19,5 +28,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/path-variable/{name}")
     public helloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
         return new  helloWorldBean(String.format ("Hello World , %s", name));
+    }
+    @GetMapping(path = "/hello-world-internationalization")
+    public String helloWorldInternationalization() {
+        Locale locale = LocaleContextHolder.getLocale();
+       return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
     }
 }
