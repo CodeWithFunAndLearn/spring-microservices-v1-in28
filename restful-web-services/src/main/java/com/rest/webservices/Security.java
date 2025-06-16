f21826d156ea39df98e70384bf09d61b42ae292f
Anurag Lal <anurag1995lal@gmail.com>
2025-06-16 23:38:29 +0530

    Basic Auth fix for POST APIes

---
+++ restful-web-services/src/main/java/com/rest/webservices/Security.java
@@ -0,0 +1,25 @@
+package com.rest.webservices;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class Security {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(withDefaults()) // Basic Auth for API clients (Postman)
+                .csrf(csrf -> csrf.disable()); // Disable CSRF for non-browser clients
+        return http.build();
+    }
+}
