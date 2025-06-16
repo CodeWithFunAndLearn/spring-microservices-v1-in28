68759bf2bc616b973d359f297215f47d93b77ba1
Anurag Lal <anurag1995lal@gmail.com>
2025-06-16 23:39:31 +0530

    Removed imports

--- restful-web-services/src/main/java/com/rest/webservices/Security.java
+++ restful-web-services/src/main/java/com/rest/webservices/Security.java
@@ -3,10 +3,6 @@ package com.rest.webservices;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
