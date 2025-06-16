022f552fc6d910e73e94e66ab3365ce7ebdd7156
Anurag Lal <anurag1995lal@gmail.com>
2025-06-16 20:18:56 +0530

    Rest API for posts.

---
+++ restful-web-services/src/main/java/com/rest/webservices/user/PostRepostory.java
@@ -0,0 +1,6 @@
+package com.rest.webservices.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepostory extends JpaRepository<Post,Integer> {
+}
--- restful-web-services/src/main/java/com/rest/webservices/user/UserJpaController.java
+++ restful-web-services/src/main/java/com/rest/webservices/user/UserJpaController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,12 +19,12 @@ import java.util.Optional;
 @RestController
 public class UserJpaController {
 
-    private UserDaoService userDaoService;
+    private PostRepostory postRepostory;
 
     private UserRepostory userRepostory;
 
-    public UserJpaController(UserDaoService userDaoService, UserRepostory userRepostory) {
-        this.userDaoService = userDaoService;
+    public UserJpaController(PostRepostory postRepostory, UserRepostory userRepostory) {
+        this.postRepostory = postRepostory;
         this.userRepostory = userRepostory;
     }
 
@@ -55,7 +56,13 @@ public class UserJpaController {
     // This method will update a user
     @PutMapping("/jpa/users/update")
     public User updateUser(User user) {
-        return userDaoService.saveUser(user);
+        // Check if the user exists
+        Optional<User> existingUser = userRepostory.findById(user.getId());
+        if (existingUser.isEmpty()) {
+            throw new UserNotFoundException("User with id " + user.getId() + " not found");
+        }
+        // Update the user
+        return userRepostory.save(user);
     }
 
     // This method will delete a user
@@ -63,5 +70,27 @@ public class UserJpaController {
     public void deleteUser(@PathVariable int id) {
             userRepostory.deleteById(id);
 
+    }
+
+    @GetMapping ("/jpa/users/{id}/posts")
+    public List<Post> getPostsForUser(@PathVariable int id) throws UserNotFoundException {
+        Optional<User> user = userRepostory.findById(id);
+        if (user.isEmpty()) 
+            throw new UserNotFoundException("User with id " + id + " not found");
+        return user.get().getPosts();
+    }
+    @PostMapping("/jpa/users/{id}/posts")
+    public ResponseEntity<Post> savePostForUser(@PathVariable int id, @Valid @RequestBody Post post) throws UserNotFoundException {
+        Optional<User> user = userRepostory.findById(id);
+        if (user.isEmpty())
+            throw new UserNotFoundException("User with id " + id + " not found");
+        post.setUser(user.get());
+        Post savedPost = postRepostory.save(post);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedPost.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(savedPost);
     }
 }
